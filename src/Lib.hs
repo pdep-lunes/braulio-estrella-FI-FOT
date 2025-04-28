2e50c7640cdea9b9ca4a9c0cd32760a06a04fedf
Fiorella Morales <mofi.mora@gmail.com>
2025-04-28 11:34:39 -0300

    etiquetación personajes

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,15 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+type NombrePersonaje = string
+type PoderBasico = string
+type nombreSuperPoder = string
+type radioSuperPoder = Int
+type Actividad = bool
+type CantidadDeVida = Int
+
+type Personaje = (NombrePersonaje, PoderBasico, nombreSuperPoder, radioSuperPoder, Actividad, CantidadDeVida)
+
+espina :: Personaje
+espina = ("Espina", "Bola de Espinas", "Granada de Espinas", 5, True, 4800)
